d848057fa9209c9be1e85d6a053af063cbef585c
Eric Tuchscherer <eric@tuchscherer.me>
2021-06-24 12:10:22 -0700

    Fixing lint error

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import Dashboard from '../components/Dashboard';
+import { UserModel } from '../types/index';
 
-export default function HomeScreen({ navigation, currentUser }): JSX.Element {
+export default function HomeScreen({ currentUser }: { currentUser: UserModel }): JSX.Element {
 
-  return (<Dashboard currentUser={currentUser} navigation={navigation} />)
+  return (<Dashboard currentUser={currentUser} />)
 }
