7423f80817232610c05de8f62abd29ce9b626edf
Eric Tuchscherer <eric@tuchscherer.me>
2021-06-25 12:07:43 -0700

    adding ry as a mocked user

--- data/mock/users.ts
+++ data/mock/users.ts
@@ -3,7 +3,13 @@ const Users = [{
   "password": "password",
   "firstName": "Eric",
   "lastName": "Tuchscherer",
-  "avatar": "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.6435-1/s720x720/158354206_10159193936639521_893875643224724013_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=7206a8&_nc_ohc=aj1irdPWkm8AX_SwSot&_nc_ht=scontent-sjc3-1.xx&tp=7&oh=b7b24725579c77ce8555d668bdc68685&oe=60D5F76D"
+  "avatar": "https://static.wikia.nocookie.net/sulleycinematicuniverse/images/f/fd/Mr_clean.jpg/revision/latest/top-crop/width/360/height/450?cb=20190704024657"
+}, {
+  "email": "ry.robinson@classmates.com",
+  "password": "password",
+  "firstName": "Ry",
+  "lastName": "Robinson",
+  "avatar": "https://www.hollywoodreporter.com/wp-content/uploads/2021/06/M2DBAFO_EC002-H-2021.jpg?w=681&h=383&crop=1"
 }];
 
 export default Users;
